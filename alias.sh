d6bf057e99d055a4a1beb992bbe547925cdde084
Nick Chan <nick.chan@utexas.edu>
2025-03-14 13:14:31 -0500

    Added medium resolution, synronized camera pose to map placement

--- alias.sh
+++ alias.sh
@@ -1,6 +1,7 @@
 # Commands to run inside the container
-alias run_gazebo_high='ros2 launch sterling_gazebo sidewalks.launch.py high_res:=True'
-alias run_gazebo_low='ros2 launch sterling_gazebo sidewalks.launch.py high_res:=False'
+alias run_gazebo_low='ros2 launch sterling_gazebo sidewalks.launch.py res:=low'
+alias run_gazebo_medium='ros2 launch sterling_gazebo sidewalks.launch.py res:=medium'
+alias run_gazebo_high='ros2 launch sterling_gazebo sidewalks.launch.py res:=high'
 alias run_nav2='ros2 launch husarion_nav2 navigation2_bringup.launch.py use_rviz:=True use_sim_time:=True nav2_config_file_slam:=/root/ros2_ws/src/sterling/config/nav2_params.yaml'
 alias run_sterling_costmap='source /root/ros2_ws/install/setup.bash && ros2 launch sterling costmaps.launch.py'
 alias save_costmap='source /root/ros2_ws/install/setup.bash && ros2 service call /sterling/save_costmap std_srvs/srv/Trigger'
