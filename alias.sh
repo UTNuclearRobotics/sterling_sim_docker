97e66f0a6894de36d201f23fc4a0e88f08e035c9
Andrew Sharp <asharp@utexas.edu>
2025-03-19 19:24:58 -0500

    Converting to the changed name of the Husarian packages.

--- alias.sh
+++ alias.sh
@@ -1,6 +1,6 @@
 # Commands to run inside the container
-alias run_gazebo_high='ros2 launch sterling_gazebo sidewalks.launch.py high_res:=True'
-alias run_gazebo_low='ros2 launch sterling_gazebo sidewalks.launch.py high_res:=False'
-alias run_nav2='ros2 launch husarion_nav2 navigation2_bringup.launch.py use_rviz:=True use_sim_time:=True nav2_config_file_slam:=/root/ros2_ws/src/sterling/config/nav2_params.yaml'
+alias run_gazebo_high='ros2 launch sterling_gazebo sidewalks.launch.py high_res:=True namespace:=panther'
+alias run_gazebo_low='ros2 launch sterling_gazebo sidewalks.launch.py high_res:=False namespace:=panther'
+alias run_nav2='ros2 launch utexas_panther bringup_launch.py namespace:=panther observation_topic:=ouster/scan observation_topic_type:=laserscan slam:=true use_sim_time:=true'
 alias run_sterling_costmap='source /root/ros2_ws/install/setup.bash && ros2 launch sterling costmaps.launch.py'
 alias save_costmap='source /root/ros2_ws/install/setup.bash && ros2 service call /sterling/save_costmap std_srvs/srv/Trigger'
